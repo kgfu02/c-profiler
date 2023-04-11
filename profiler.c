e9cf94e73e5761e0430e34e0130bbcfd9cde39eb
Kevin Fu <kevingf2@example.com>
2023-04-11 18:05:04 -0500

    pauses child after exec call

--- profiler.c
+++ profiler.c
@@ -3,14 +3,43 @@
 #include <sys/ptrace.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <sys/user.h>
 #include <time.h>
+#include <signal.h>
+#include <syscall.h>
 
-int main()
-{
+struct user_regs_struct regs;
+struct timespec start;
+
+double get_elapsed_time() {
+    struct timespec end;
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    return (end.tv_sec - start.tv_sec) +
+                   (double)(end.tv_nsec - start.tv_nsec) / 1000000000.0;
+}
+
+void resume_until_exec(pid_t pid) {
+    // Resume child until it reaches the exec call
+    while (1) {
+        ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+        int status;
+        waitpid(pid, &status, 0);
+        if (WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP) {
+            // Check if the child has made the exec call
+            ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+            if (regs.orig_rax == SYS_execve) {
+                break;
+            }
+        }
+    }
+}
+
+int main() {
     pid_t child_pid;
     int status;
 
     child_pid = fork();
+    clock_gettime(CLOCK_MONOTONIC, &start);
     if (child_pid == -1) {
         perror("fork");
         exit(EXIT_FAILURE);
@@ -18,20 +47,21 @@ int main()
 
     if (child_pid == 0) {
         // Child process
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         execl("./test", "test", NULL);
         exit(1);
     } else {
         // Parent process
         printf("Child process started, pid = %d\n", child_pid);
-        ptrace(PTRACE_SEIZE, child_pid, NULL, NULL);
-        sleep(1);
-        printf("Child process paused\n");
+        resume_until_exec(child_pid);
         ptrace(PTRACE_INTERRUPT, child_pid, NULL, NULL);
-        sleep(10);
-        ptrace(PTRACE_DETACH, child_pid, NULL, NULL); // Detach from child process
-        printf("Child process resumed\n");
-        waitpid(child_pid, &status, 0); // Wait for child to exit
-        printf("Child process exited with status %d\n", WEXITSTATUS(status));
+        printf("Child process paused...\n");
+        sleep(5);
+        ptrace(PTRACE_DETACH, child_pid, NULL,
+               NULL);  // Detach from child process
+        printf("Child process resumed...\n");
+        waitpid(child_pid, &status, 0);  // Wait for child to exit
+        printf("Child ran for %f secs.", get_elapsed_time());
         exit(EXIT_SUCCESS);
     }
 }
--- test.c
+++ test.c
@@ -11,12 +11,11 @@ int main() {
 
     clock_gettime(CLOCK_MONOTONIC, &start);
     printf("Sleeping!\n");
-    sleep(5);
+    sleep(1);
     printf("Done sleeping!\n");
     clock_gettime(CLOCK_MONOTONIC, &end);
 
     elapsed_time = (end.tv_sec - start.tv_sec) +
                    (double)(end.tv_nsec - start.tv_nsec) / 1000000000.0;
-    printf("Elapsed time: %f seconds\n", elapsed_time);
     return 0;
 }
